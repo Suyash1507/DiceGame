7987416763138d32a351d94ce5db22b6f19c6f77
Suyash1507 <64803486+Suyash1507@users.noreply.github.com>
2021-08-13 16:35:12 +0530

    Update index.js

--- Dice game/index.js
+++ Dice game/index.js
@@ -11,4 +11,13 @@ var randomDiceChange2= "dice" + randomNumber2 + ".png";
 var randomDiceImage2= "images/" + randomDiceChange2;
 var image2= document.querySelectorAll("img")[1];
 image2.setAttribute("src", randomDiceImage2);
-
+//Winner 
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML = "🚩Player 1 wins!!";
+}
+else if((randomNumber2>randomNumber1)){
+    document.querySelector("h1").innerHTML= "🚩Player 2 wins!!";
+}
+else{
+    document.querySelector("h1").innerHTML= "Draw !!";
+}
